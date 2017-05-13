e67425cbca77863cb34a6e9d1da41003283972a4
Nikita <fordf150svtraptor@gmail.com>
2017-05-13 19:14:23 +0300

    Bug fixes

--- MainServer/AutoApp/urls.py
+++ MainServer/AutoApp/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import url
 from AutoApp import views
 
 urlpatterns = [
-    url(r'load$', views.load)
+    url(r'load$', views.load),
+    url(r'parse$', views.parse_test)
 ]
--- MainServer/AutoApp/views.py
+++ MainServer/AutoApp/views.py
@@ -1,12 +1,29 @@
 from django.http import HttpResponse
 
 from AutoApp.db_filling import fill_database
-
+from AutoApp.parser import parse
 
 # Create your views here.
 
 
 def load(request):
     fill_database()
-
     return HttpResponse("hello, files loading success")
+
+
+def parse_test(request):
+    r = "https://auto.ria.com/search/?" \
+          "category_id=1&" \
+          "marka_id=84&" \
+          "model_id=30786&" \
+          "state%5B0%5D=10&" \
+          "s_yers%5B0%5D=0&" \
+          "po_yers%5B0%5D=0&" \
+          "price_ot=&" \
+          "price_do=&" \
+          "currency=1&" \
+          "countpage=100"
+    # parser.parse(r)
+    return HttpResponse(parse(r))
+
+
