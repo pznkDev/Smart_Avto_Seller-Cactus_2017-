5e5e80ef108688bfc68a2071eba3bb87327d4d79
Nikita <fordf150svtraptor@gmail.com>
2017-05-13 13:40:05 +0300

    Init

---
+++ MainServer/AutoApp/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Mark(models.Model):
+    value_id = models.CharField(max_length=200)
+    name = models.CharField(max_length=200)
+
+
+class Model(models.Model):
+    value_id = models.CharField(max_length=200)
+    mark = models.ForeignKey(Mark)
+    name = models.CharField(max_length=200)
+
+
+class Region(models.Model):
+    value_id = models.CharField(max_length=200)
+    name = models.CharField(max_length=200)
